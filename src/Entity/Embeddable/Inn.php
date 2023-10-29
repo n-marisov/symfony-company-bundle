da548201d7ff32c05aa7c6a2163acb55ce56c0c2
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-10-29 12:12:39 +0300

    25

--- src/Entity/Embeddable/Inn.php
+++ src/Entity/Embeddable/Inn.php
@@ -24,7 +24,7 @@ class Inn implements Stringable
      * Значение ИНН хранится как строка.
      * @var string
      */
-    #[Column(name: 'inn',length: '12')]
+    #[Column(name: 'inn',length: '12',unique: true)]
     protected string $value;
 
     /**
