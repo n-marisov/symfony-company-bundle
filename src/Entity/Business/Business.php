212cf28be2af1ef156047901164dc3979868ca83
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-10-29 09:55:26 +0300

    18

--- src/Entity/Business/Business.php
+++ src/Entity/Business/Business.php
@@ -17,7 +17,7 @@ use Maris\Symfony\Company\Traits\EntityIdentifierTrait;
  */
 #[Entity]
 #[Table(name: 'business',uniqueConstraints: [
-    new UniqueConstraint(columns: ['bik'], fields: ['bik'])
+    new UniqueConstraint(name:'bik_index',columns: ['bik'], fields: ['bik'])
 ] )]
 #[InheritanceType('SINGLE_TABLE')]
 #[DiscriminatorColumn(name: 'business_type',type: 'integer')]
