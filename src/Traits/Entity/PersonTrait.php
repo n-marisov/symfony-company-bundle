de52d6095e5485b39cfc62e21727f1d9d682148c
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-11-01 15:51:34 +0300

    49

--- src/Traits/Entity/PersonTrait.php
+++ src/Traits/Entity/PersonTrait.php
@@ -4,13 +4,28 @@ namespace Maris\Symfony\Company\Traits\Entity;
 
 use Doctrine\ORM\Mapping\JoinColumn;
 use Doctrine\ORM\Mapping\OneToOne;
+use Maris\Symfony\Company\Entity\Business\Business;
 use Maris\Symfony\Person\Entity\Person;
 use Maris\Symfony\Person\Traits\PersonAggregateNotNullTrait;
 
+/***
+ * @extends Business
+ */
 trait PersonTrait
 {
-    use PersonAggregateNotNullTrait;
+    //use PersonAggregateNotNullTrait;
     /*#[OneToOne(targetEntity: Person::class,cascade: ['persist'])]
     #[JoinColumn(name: 'person_id')]
     protected Person $person;*/
+
+    public function getPerson():Person
+    {
+        return $this->person;
+    }
+
+    public function setPerson( Person $person ):static
+    {
+        $this->person = $person;
+        return $this;
+    }
 }
