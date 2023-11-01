3502b254aeb684b516f58541ff31018c3a3b9241
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-11-01 19:40:17 +0300

    53

--- src/Repository/Business/BusinessRepository.php
+++ src/Repository/Business/BusinessRepository.php
@@ -25,6 +25,14 @@ use TypeError;
  */
 class BusinessRepository extends ServiceEntityRepository
 {
+    /****
+     * Типы данных с которыми работает репозиторий.
+     */
+    protected const BUSINESS_TYPES = [
+        Physical::class, Employed::class,Entrepreneur::class,Company::class,Bank::class
+    ];
+
+
     /***
      * Сервис модифицирующий QueryBuilder
      * @var QueryBuilderModifier
@@ -77,52 +85,38 @@ class BusinessRepository extends ServiceEntityRepository
      */
     public function findLike(string $value , array $fields = [], array $types = [], ?int $limit = null, ?int $offset = null, ?array $orderBy = null):array
     {
-
+        $value =  "%".trim($value)."%";
         $aliases = [
             "inn" => "inn.value",
             "kpp" => "kpp.value",
             "ogrn" => "ogrn.value",
+            "bik" => "bik.value",
             "title" => "title",
         ];
 
-        if(empty($fields))
-            $fields = ["inn","ogrn"];
+        $builder =  $this->createQueryBuilder('b');
 
-        if(empty($types))
-            $types = [ "PHYSICAL", "EMPLOYED", "ENTREPRENEUR", "COMPANY", "BANK" ];
-
-        $builder =  $this->createQueryBuilder('c');
-
-        $builder = new QueryBuilder( $this->getEntityManager() );
-        $builder->select("f","s","i","c","b");
-        $builder->from(Physical::class, "f");
-        $builder->from(Employed::class, "s");
-        $builder->from(Entrepreneur::class, "i");
-        $builder->from(Company::class, "c");
-        $builder->from(Bank::class, "b");
-
-
-        $newFields = [];
-        foreach ( $fields as $field )
-            if(array_key_exists($field,$aliases))
-                $newFields[$field] = $aliases[$field];
-        $fields = $newFields;
-
-        foreach ( $fields as $field => $fieldAddress ){
-            if(is_null($builder->getParameter("like_$field")))
-                $builder->setParameter("like_$field","%$value%");
-
-            if(in_array($field,["inn","ogrn"])){
-                $builder->orWhere("i.$fieldAddress LIKE :like_$field");
+        $whereFields = $builder->expr()->orX();
+        foreach ( $fields as $field ){
+            if(array_key_exists($field,$aliases)){
+                $whereFields->add( $builder->expr()->like("b.{$aliases[$field]}",":$field") );
+                $builder->setParameter( $field, $value );
             }
         }
 
+        $typesWrite = $builder->expr()->orX();
 
-       // $this->modifierBuilder->modifyOrderBy("c", $builder, $orderBy);
+        foreach ($types as $type){
+            if(in_array($type,self::BUSINESS_TYPES))
+                $typesWrite->add("b  INSTANCE OF $type");
+        }
+
+
+        $builder->andWhere($whereFields)->andWhere($typesWrite);
+
+        $this->modifierBuilder->modifyOrderBy("c", $builder, $orderBy);
         $this->modifierBuilder->modifyLimit( $builder, $limit );
         $this->modifierBuilder->modifyOffset($builder, $offset );
-
-       //dump($builder->getQuery()->getSQL());
 
         return $builder->getQuery()->getResult();
     }
